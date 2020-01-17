6b229f827a161a91f5db5370587d0bfd0b707dfb
noomdalv <vladlunasan@gmail.com>
2020-01-17 15:49:09 -0500

    fixed makeMove function

--- js/main.js
+++ js/main.js
@@ -3,11 +3,10 @@ const gameBoard = (() => {
 	//Game Setup
 
   let gamePositions;
-  let onclick;
+  let btnClick;
   let movesCounter;
-  let player1;
-  let player2;
-  let currentPlayer;
+	let player1, player2;
+	let currentPlayer;
 
   const winningCombos = [
     ['one', 'two', 'three'],
@@ -20,44 +19,48 @@ const gameBoard = (() => {
     ['three','five','seven']
   ];
 
-  const startGame = () => {
-    gamePositions = {
-      one: '',
-      two: '',
-      three: '',
-      four: '',
-      five: '',
-      six: '',
-      seven: '',
-      eight: '',
-      nine: ''
-    };
-
-    onclick = {
-      one: 'makeMove(one)',
-      two: 'makeMove(two)',
-      three: 'makeMove(three)',
-      four: 'makeMove(four)',
-      five: 'makeMove(five)',
-      six: 'makeMove(six)',
-      seven: 'makeMove(seven)',
-      eight: 'makeMove(eight)',
-      nine: 'makeMove(nine)',
-    };
-
-    movesCounter = 0;
-
-    player1 = playerFactory(false, "X");
-    player2 = playerFactory(false, "O");
-    currentPlayer = player1;
-
-    renderGameBoard();
-  }
-
-
 	const render = function (template, node) {
 	  node.innerHTML = template;
 	};
+
+	//Player functions
+
+	const playerFactory = (name,mark) => {
+	  return { name, mark }
+	}
+
+	// createPlayer function
+
+	const playerCreation = () => {
+		let p1Name = document.getElementById('player1_name').value
+		let p2Name = document.getElementById('player2_name').value
+
+		player1.name = p1Name
+		player2.name = p2Name
+		renderGameBoard();
+	}
+
+	//Game Logic functions
+
+	const makeMove = (buttonId) => {
+		if (player1.name === "" || player2.name === "") {
+			let errorMessage = `<div class='error'>
+														<h3>You need to submit your name to start the game!</h3>
+													</div>`
+			renderGameBoard(errorMessage);
+		}
+
+		btnClick[buttonId] = '';
+		gamePositions[buttonId] = currentPlayer.mark;
+		movesCounter++;
+		console.log(gamePositions);
+		checkWin()
+		if (movesCounter === 9) {
+			displayResults('draw');
+		}
+		switchPlayer();
+		renderGameBoard();
+	}
 
 	const renderGameBoard = (resultsTemplate = "") => {
 
@@ -68,74 +71,43 @@ const gameBoard = (() => {
 												<label for="player2_name">Player O: </label>
 												<input type="text" id="player2_name"></input>
 
-												<button onclick="playerCreation()">Start</button>
+												<button onclick="gameBoard.playerCreation()">Start</button>
 											</div>`
 
     let gameBoard = `<div id='gameboard'>
                       <div>
-                        <button onclick=${onclick.one} id='one'>${gamePositions.one}</button>
-                        <button onclick=${onclick.two}  id='two'>${gamePositions.two}</button>
-                        <button onclick=${onclick.three}  id='three'>${gamePositions.three}</button>
+                        <button onclick=${btnClick.one} id='one'>${gamePositions.one}</button>
+                        <button onclick=${btnClick.two}  id='two'>${gamePositions.two}</button>
+                        <button onclick=${btnClick.three}  id='three'>${gamePositions.three}</button>
                       </div>
                       <div>
-                        <button onclick=${onclick.four}  id='four'>${gamePositions.four}</button>
-                        <button onclick=${onclick.five}  id='five'>${gamePositions.five}</button>
-                        <button onclick=${onclick.six}  id='six'>${gamePositions.six}</button>
+                        <button onclick=${btnClick.four}  id='four'>${gamePositions.four}</button>
+                        <button onclick=${btnClick.five}  id='five'>${gamePositions.five}</button>
+                        <button onclick=${btnClick.six}  id='six'>${gamePositions.six}</button>
                       </div>
                       <div>
-                        <button onclick=${onclick.seven}  id='seven'>${gamePositions.seven}</button>
-                        <button onclick=${onclick.eight}  id='eight'>${gamePositions.eight}</button>
-                        <button onclick=${onclick.nine}  id='nine'>${gamePositions.nine}</button>
+                        <button onclick=${btnClick.seven}  id='seven'>${gamePositions.seven}</button>
+                        <button onclick=${btnClick.eight}  id='eight'>${gamePositions.eight}</button>
+                        <button onclick=${btnClick.nine}  id='nine'>${gamePositions.nine}</button>
                       </div>
                     </div>`;
-										
-    if (player1.name !== false && player2.name !== false) {
+
+    if (player1.name && player2.name) {
       renderTemplate = gameBoard + resultsTemplate;
     } else {
       renderTemplate = playerForm + gameBoard + resultsTemplate;
     }
 	  render(renderTemplate, document.querySelector('#container'));
-	  console.log(template);
   };
-
-
-	//Player functions
-
-	const playerFactory = (name,mark) => {
-	  return { name, mark }
-	}
-
-
-	//Game Logic functions
-
-  const makeMove = (buttonId) => {
-    if (player1.name === false || player2.name === false) {
-      let errorMessage = `<div class='error'>
-                            <h3>You need to submit your name to start the game!</h3>
-                          </div>`
-      renderGameBoard(errorMessage);
-    }
-
-    onclick[buttonId] = '';
-    gamePositions[buttonId] = currentPlayer.mark;
-    movesCounter++;
-    checkWin()
-    if (movesCounter === 9) {
-      displayResults('draw');
-    }
-    switchPlayer();
-    renderGameBoard();
-  }
 
   // checkWin function
 
   const checkWin = () => {
-    let currMark = currentPlayer.mark
     for (let i = 0; i < winningCombos.length; i++) {
-      if (gamePositions[winningCombos[i][0]] === currMark &&
-          gamePositions[winningCombos[i][1]] === currMark &&
-          gamePositions[winningCombos[i][2]] === currMark) {
-            displayResults('won',currentPlayer);
+      if (gamePositions[winningCombos[i][0]] === currentPlayer.mark &&
+          gamePositions[winningCombos[i][1]] === currentPlayer.mark &&
+          gamePositions[winningCombos[i][2]] === currentPlayer.mark) {
+            displayResults('won', currentPlayer);
           };
       }
   }
@@ -143,9 +115,7 @@ const gameBoard = (() => {
 
 	// switchPlayer function
 
-	const switchPlayer = () => {
-		(currentPlayer === player1) ? currentPlayer = player2 : currentPlayer = player1
-	}
+	const switchPlayer = () => { (currentPlayer === player1) ? currentPlayer = player2 : currentPlayer = player1 }
 
 	// displayResult function
 	const displayResults = (result, player) => {
@@ -164,15 +134,46 @@ const gameBoard = (() => {
     };
 	}
 
-	// createPlayer function
+	const startGame = () => {
+		gamePositions = {
+			one: '',
+			two: '',
+			three: '',
+			four: '',
+			five: '',
+			six: '',
+			seven: '',
+			eight: '',
+			nine: ''
+		};
 
-	const playerCreation = () => {
-		let p1Name = document.getElementById('player1_name').value
-		let p2Name = document.getElementById('player2_name').value
+		btnClick = {
+			one: "gameBoard.makeMove('one')",
+			two: "gameBoard.makeMove('two')",
+			three: "gameBoard.makeMove('three')",
+			four: "gameBoard.makeMove('four')",
+			five: "gameBoard.makeMove('five')",
+			six: "gameBoard.makeMove('six')",
+			seven: "gameBoard.makeMove('seven')",
+			eight: "gameBoard.makeMove('eight')",
+			nine: "gameBoard.makeMove('nine')",
+		};
 
-		player1.name = p1Name
-		player2.name = p2Name
+		movesCounter = 0
+
+		player1 = playerFactory("", "X");
+		player2 = playerFactory("", "O");
+		currentPlayer = player1;
+
+		renderGameBoard();
 	}
 
-	startGame();
+	return {
+		startGame,
+		playerCreation,
+		makeMove
+	}
+
 })();
+
+gameBoard.startGame();
